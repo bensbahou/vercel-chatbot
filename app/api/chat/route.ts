f17a0cb61272a0ce8cdf3326d353d922c01cb446
bensbahou <younesbensbahou@gmail.com>
2023-10-15 09:47:58 +0100

    no saving to db

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,9 +1,9 @@
-import { kv } from '@vercel/kv'
+// import { kv } from '@vercel/kv'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
 
 import { auth } from '@/auth'
-import { nanoid } from '@/lib/utils'
+// import { nanoid } from '@/lib/utils'
 
 export const runtime = 'edge'
 
@@ -37,29 +37,29 @@ export async function POST(req: Request) {
 
   const stream = OpenAIStream(res, {
     async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
+      // const title = json.messages[0].content.substring(0, 100)
+      // const id = json.id ?? nanoid()
+      // const createdAt = Date.now()
+      // const path = `/chat/${id}`
+      // const payload = {
+      //   id,
+      //   title,
+      //   userId,
+      //   createdAt,
+      //   path,
+      //   messages: [
+      //     ...messages,
+      //     {
+      //       content: completion,
+      //       role: 'assistant'
+      //     }
+      //   ]
+      // }
+      // await kv.hmset(`chat:${id}`, payload)
+      // await kv.zadd(`user:chat:${userId}`, {
+      //   score: createdAt,
+      //   member: `chat:${id}`
+      // })
     }
   })
 
--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,12 +6,12 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'Who is Bensbahou?',
+    message: `Who is Bensbahou?`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'Tell me a joke!',
+    message: 'Tell me a joke!'
   },
   {
     heading: 'Draft an email',
@@ -24,15 +24,10 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Welcome to This dev preview!
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
+          This is a preview for demonstration.
         </p>
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
