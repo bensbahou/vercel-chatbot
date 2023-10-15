0d21df4b4b89cb00492a7fef3510359181008c33
bensbahou <younesbensbahou@gmail.com>
2023-10-15 13:14:09 +0100

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { Header } from '@/components/header'
 
 export const metadata: Metadata = {
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Avasana',
+    template: `%s - Avasana`
   },
   description: 'An AI-powered chatbot template built with Next.js and Vercel.',
   themeColor: [
